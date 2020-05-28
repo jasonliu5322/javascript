f7874efad14e9e2642986a4927c31200915ec7f1
Hongliang Liu <jasonliu5322@gmail.com>
2020-05-28 14:28:47 -0700

    update

--- js/add-content.js
+++ js/add-content.js
@@ -17,13 +17,33 @@ var name = window.prompt("What is your name?");
 var place = window.prompt("Where are you living?")
 
 function append(var1, var2){
-    var var3 = var1 + var2; 
-    var newMessage = document.getElementById('message3');
-    if(var1 == 'null' || var2 == null || var1 == '' || var2 == '' ){ 
-       newMessage.textContent = ("Your input is invalid, refresh this page.");
-    }else{ newMessage.textContent = var3 + " is a name of place?";}  
+    var flag = true;
+    while(flag){
+        var var3 = var1 + var2; 
+        var newMessage = document.getElementById('message3');
+        if(var1 == null || var2 == null || var1 == '' || var2 == ''){ 
+            newMessage.textContent = ("Your input is invalid, refresh this page.");
+            var name1 = window.prompt("What is your name?");// Prompt user input again
+            var place1 = window.prompt("Where are you living?");
+            var1 = name1; var2 = place1;// Set new value to arguments
+        }else{ 
+            newMessage.textContent = var3 + " is a name of place?";
+            flag = false;// Set condition to false
+        }  
+   }name = var1; place = var2;// For cases with more than one input 
 }
 append(name, place);
+
+var num = window.prompt("How many numbers do you want to add to this page?")
+function addNumber(number){
+    var newMessage = document.getElementById('message3');
+    var Message = '';
+    for(var i = 0; i < number; i++){
+        Message +=  ((i + 1) + ' ');
+    }
+    newMessage.textContent = "Your number list:" + Message;
+}
+addNumber(num);
 
 function updateMessage(){
     var mes = "Hi " + name + ", thanks for visiting my page!^-^"
