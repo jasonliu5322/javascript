5666611a94504d6c87c3e41920c82b9b88912803
Hongliang Liu <jasonliu5322@gmail.com>
2020-05-29 15:39:26 -0700

    update

--- js/add-content.js
+++ js/add-content.js
@@ -1,3 +1,4 @@
+
 var today= new Date();
 var hourNow = today.getHours(); var greeting;
 var greeting;
@@ -13,26 +14,63 @@ if (hourNow > 18) {
 }
 document.write( '<h3>' + greeting + '</h3>');
 
-var name = window.prompt("What is your name?");
-var place = window.prompt("Where are you living?")
+/*var user_name, place; 
+user_name = window.prompt("What is your name?");
+//console.log(user_name);
+//console.log(place);
 
 function append(var1, var2){
-    var flag = true;
-    while(flag){
-        var var3 = var1 + var2; 
-        var newMessage = document.getElementById('message3');
-        if(var1 == null || var2 == null || var1 == '' || var2 == ''){ 
-            newMessage.textContent = ("Your input is invalid, refresh this page.");
-            var name1 = window.prompt("What is your name?");// Prompt user input again
-            var place1 = window.prompt("Where are you living?");
-            var1 = name1; var2 = place1;// Set new value to arguments
-        }else{ 
-            newMessage.textContent = var3 + " is a name of place?";
-            flag = false;// Set condition to false
-        }  
-   }name = var1; place = var2;// For cases with more than one input 
+    var flag1 = true;
+    var flag2 = true;
+    while(flag1){
+        //var var3 = var1 + var2; 
+        //var newMessage = document.getElementById('message3');
+        if (var1 == null ||  var1 == ''){ 
+            //newMessage.textContent = ("Your input is invalid, refresh this page.");
+            var name1 = window.prompt("Your input is invalid, please input your name again?");// Prompt user input again
+            var1 = name1; // Set new value to arguments
+        } else { 
+            //newMessage.textContent = var3 + " is a name of place?";
+            flag1 = false;// Set condition to false
+        }
+   }
+   place = window.prompt("Where are you living?");
+   var2 = place;
+   while(flag2){
+       if(var2 == null || var2 == ''){
+             var place1 = window.prompt("Your input is invalid, please input your place again?");
+             var2 = place1;
+       }else { 
+        flag2 = false;
+    }
+   }
+   user_name = var1; 
+   place = var2;// For cases with more than one input 
 }
-append(name, place);
+append(user_name, place);*/
+
+var user_name, place;
+function append(){
+    var flag1 = true;
+    var flag2 = true;
+    user_name = window.prompt("What is your name?");//prompt user input name
+    while(flag1){
+        if (user_name == null ||  user_name == ''){ 
+            user_name = window.prompt("Your input is invalid, please input your name again?");// Prompt user input again
+        } else {             
+            flag1 = false;
+        }
+    }
+    place = window.prompt("Where are you living?");//prompt user input place
+    while(flag2){
+        if(place == null || place == ''){
+            place = window.prompt("Your input is invalid, please input your place again?");
+        }else { 
+            flag2 = false;
+        }
+    }
+ }
+append();
 
 var num = window.prompt("How many numbers do you want to add to this page?")
 function addNumber(number){
@@ -46,7 +84,7 @@ function addNumber(number){
 addNumber(num);
 
 function updateMessage(){
-    var mes = "Hi " + name + ", thanks for visiting my page!^-^"
+    var mes = "Hi " + user_name + ", thanks for visiting my page!^-^"
     var mes1 = place + " is an amazing place!"
     var newMessage = document.getElementById('message');
     newMessage.textContent = mes;
